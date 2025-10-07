31ec18f43e5d970bcf6d853a71e02cb7d5f506dd
Agent-Jalovec <tomaspetrica@gmail.com>
2025-10-07 14:48:28 +0200

    nedokonceno a nefunguje

---
+++ bitva/lod.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+'''
+vO.0.1
+Lod a odvozene tridy pro vesmirny souboj.
+'''
+
+class Lod:
+    '''
+    Zakladni trida reprezentujici lod.
+    '''
+
+    def __init__(self, jmeno, trup, utok, stit, kostka)
+    self._jmeno = jmeno
+    self._trup = trup
+    self._max_trup = trup
+    self._utok = utok
+    self._stit = stit
+    self._kostka = kostka
+    self._zprava = ''
+
+    def __str__(self):
+        return str(self._jmeno)
+
+    def utoc(self. souper):
+        uder = self.utok + self._kostka.hod()
+        zprava = f'{self._jmeno} pali kanony za {uder} hp!'
+        self.nastav_zpravu(zprava)
+        souper.bran_se(uder)
+
+    def bran_se(self, uder):
+        poskozeni = uder - (self._stit + self._kostka.hod())
+        if poskozeni > 0:
+            zprava = f'{self._jmeno} utrpela zasah o sile {uder} hp!'
+            self.trup -= poskozeni
+            if self._trup < 0:
+                self._trup = 0
+                zprava = f'{zprava[:-1]} a byla znicena!'
+
+    def nastav_zpravu(self.zprava)
